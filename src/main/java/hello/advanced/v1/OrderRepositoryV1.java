6a610a7bdb1d22f3824516e210e2618ca6a83571
juhee <fhqj21@gmail.com>
2022-11-13 23:55:09 +0900

    add v1

--- src/main/java/hello/advanced/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,12 +9,25 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
-    public void save(String itemId){
-        if(itemId.equals("ex")){
-            throw new IllegalStateException("예외 발생!!");
-        }
+    private final HelloTraceV1 trace;
 
-        sleep(1000);
+    public void save(String itemId){
+
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderRepository.save()");
+
+            if(itemId.equals("ex")){
+                throw new IllegalStateException("예외 발생!!");
+            }
+
+            sleep(1000);
+
+            trace.end(status);
+        }catch (Exception e){
+            trace.exception(status,e);
+            throw e;
+        }
     }
 
     private void sleep(int millis){

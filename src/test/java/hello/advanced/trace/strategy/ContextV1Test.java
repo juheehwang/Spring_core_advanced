2ac20248768ea8a2ae3cd9da8628d69a364a8dcf
juhee <fhqj21@gmail.com>
2022-11-24 00:31:00 +0900

    learn about strategy pattern

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,10 +1,13 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class ContextV1Test {
 
     @Test
@@ -15,6 +18,38 @@ public class ContextV1Test {
 
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV2(){
+        Strategy strategy1 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 실행 1");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategy1);
+        contextV1.execute();
+
+        Strategy strategy2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 실행 2");
+            }
+        };
+        ContextV1 contextV2 = new ContextV1(strategy2);
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV3(){
+        ContextV1 contextV1 = new ContextV1(()->log.info("비즈니스 로직 1 실행"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(()->log.info("비즈니스 로직 1 실행"));
         contextV2.execute();
     }
 }

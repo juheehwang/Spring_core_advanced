ad852740afa6393a4a9d5fed38c25f2b6c841b6a
juhee <fhqj21@gmail.com>
2022-11-24 00:21:03 +0900

    learn about strategy pattern

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,20 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import org.junit.jupiter.api.Test;
+
+public class ContextV1Test {
+
+    @Test
+    void strategyV1(){
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+}
